d37857b3ef24cb88dc822baec4b4975d027cdeb1
ZevT88 <65665740+ZevT88@users.noreply.github.com>
2022-12-04 19:21:16 +0200

    Problem 21

---
+++ (21)AmicableNumbers.py
@@ -0,0 +1,24 @@
+#problem 21
+
+
+def d(n): 
+	ProperDivisors = []
+	for Devisor in range(n - 1): 
+		Devisor += 1
+		if n % Devisor == 0: 
+			ProperDivisors.append(Devisor)
+	total = 0
+	for Devisor in ProperDivisors: 
+		total += Devisor
+	return total
+	
+
+
+total = 0
+for i in range(284):
+	if i == d(d(i)):
+		total += i
+	
+
+
+print(total)
