eaebcadc1e2dcc8d8996ba4b13e2568a9d33d8c6
ZevT88 <65665740+ZevT88@users.noreply.github.com>
2023-01-13 14:54:58 +0200

    edit

--- (22)NamesScores.py
+++ (22)NamesScores.py
@@ -9,7 +9,6 @@ NamesList.sort()
 
 
 def Score(Name):
-	disallowed_characters = '"\n'
 	NewName = Name.replace('"','')
 	NewName2 = NewName.replace('\n','')
 	Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
